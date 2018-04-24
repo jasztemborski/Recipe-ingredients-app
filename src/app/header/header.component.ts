2592c5f74d761e4dbcea543b7d254b619c536896
Kuba <you@example.com>
2018-04-24 12:53:03 +0200

    components and modules

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,6 @@
    selector: 'app-header',
    templateUrl: './header.component.html'
  })
- export class HeaderComponent{
+ export class HeaderComponent {
 
  }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[] = [
+  new Recipe('A test Recipe', 'This is syply a test', 'https://www.foodiesfeed.com/wp-content/uploads/2016/01/cheers-with-cocktails.jpg'),
+  new Recipe('A test Recipe', 'This is syply a test', 'https://www.foodiesfeed.com/wp-content/uploads/2016/01/cheers-with-cocktails.jpg')
+];
   constructor() { }
 
   ngOnInit() {
