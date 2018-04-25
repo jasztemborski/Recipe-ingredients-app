d6557b0bc97a309b7179525161e634781d5efb72
Kuba <you@example.com>
2018-04-25 09:25:36 +0200

    fixed bugs

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Recipe } from '../recipe.model';
+
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-@Input() recipe: Recipe;
+  @Input() recipe: Recipe;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,16 +9,18 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-recipes: Recipe[] = [
-  new Recipe('A test Recipe', 'This is syply a test', 'https://www.foodiesfeed.com/wp-content/uploads/2016/01/cheers-with-cocktails.jpg'),
-  new Recipe('Sec test Recipe', 'This is syply a test', 'https://www.foodiesfeed.com/wp-content/uploads/2016/01/cheers-with-cocktails.jpg')
-];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-  onRecipeSelected(recipe: Recipe){
-this.recipeWasSelected.emit(recipe);
+
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
   }
 
 }
